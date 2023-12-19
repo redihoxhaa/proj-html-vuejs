34381da34e77835f4096cd5174006f780445bc6c
redihoxhaa <redi.hoxha1996@gmail.com>
2023-12-19 19:49:16 +0100

    feat: logo component

--- src/components/1Header/Logo.vue
+++ src/components/1Header/Logo.vue
@@ -1,29 +1,28 @@
 <script>
 // IMPORTS
-
-
-
+import { globalStore } from '../../globalStore';
 // /IMPORTS
-
 export default {
-    props: [],
-    components: {},
     data() {
         return {
-
+            globalStore
         }
     },
-    methods: {},
-    mounted() { },
+
 }
 </script>
 
-<template></template>
+<template>
+    <div class="custom-container">
+        <div class="pic-container">
+            <img :src="globalStore.logo.path" :alt="globalStore.logo.alt">
+        </div>
+    </div>
+</template>
 
 <style lang="scss" scoped>
-// USES
-
-
-
-// /USES
+.pic-container {
+    width: 100px;
+    padding: 40px 0;
+}
 </style>
--- src/globalStore.js
+++ src/globalStore.js
@@ -1,6 +1,13 @@
 import { reactive } from 'vue'
 
 export const globalStore = reactive({
-
+    logo: {
+        path: '/img/logo-default-slim.png',
+        alt: 'Porto Logo'
+    },
+    greyLogo: {
+        path: '/img/logo-footer.png',
+        alt: 'Porto Logo'
+    }
 });
 
